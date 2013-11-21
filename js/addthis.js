e6164c44753cb49fbac8cd2ffde89a5e72bf09fb
Yuqing Jiang <yqjiang0830@gmail.com>
2013-11-21 18:22:36 +0000

    Edit js.

--- js/addthis.js
+++ js/addthis.js
@@ -8,12 +8,12 @@
     Drupal.behaviors.delftdoraSolr = {
         attach: function(context, settings) {
             $(document).ready(function() {
-                $('.shareoptions').find('.share').find('a').addClass("addthis_button");
-                $('.shareoptions').find('.share').find('a').attr("href", "http://www.addthis.com/bookmark.php?v=300&amp;pubid=ra-5281f8c33c6dd728");
-                $('body').append('<script type="text/javascript">var addthis_config = {"data_track_addressbar":true};</script>');
-                $('body').append('<script type="text/javascript" src="//s7.addthis.com/js/300/addthis_widget.js#pubid=ra-5281f8c33c6dd728"></script>');
+                $('.shareoptions .share a')
+                        .addClass('addthis_button')
+                        .attr("href", "http://www.addthis.com/bookmark.php?v=300&amp;pubid=ra-5281f8c33c6dd728");
+                $('body').append('<script type="text/javascript">var addthis_config = {"data_track_addressbar":true};</script>' +
+                        '<script type="text/javascript" src="//s7.addthis.com/js/300/addthis_widget.js#pubid=ra-5281f8c33c6dd728"></script>');
             });
-
         }
     };
 })(jQuery);
